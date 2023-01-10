b16550b8b649315d19ed11f5a781ae71298b6a7e
eneassena10 <eneas.sena@mercadolivre.com>
2023-01-10 13:13:07 -0300

    server go sem cors chamada js é bloqueada

--- cors/main.go
+++ cors/main.go
@@ -1,39 +1,22 @@
 package main
 
 import (
-	"github.com/rs/cors"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	mux := http.NewServeMux()
-
-	cors := cors.New(cors.Options{
-		AllowedOrigins: []string{"*"},
-		AllowedMethods: []string{
-			http.MethodPost,
-			http.MethodGet,
-		},
-		AllowedHeaders:   []string{"*"},
-		AllowCredentials: false,
-	})
-
-	mux.HandleFunc("/hello", func(w http.ResponseWriter, request *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		//fmt.Fprintln(w, "Hello there! fmt")
-		w.Write([]byte("Hello"))
-	})
+	http.HandleFunc("/hello", HelloHandler)
 
 	log.Println("Listening...")
-	handler := cors.Handler(mux)
-	log.Fatal(http.ListenAndServe(":8080", handler))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-/*func HelloHandler(w http.ResponseWriter, _ *http.Request) {
+func HelloHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:80")
 	w.Header().Set("Access-Control-Max-Age", "15")
 	fmt.Println(w, "Hello, there")
 	w.Write([]byte("Hello, there"))
-}*/
+}
