e36e3b5ac759ebf9949daf341562412ba1a14f72
eneassena10 <eneas.sena@mercadolivre.com>
2023-06-20 00:26:04 -0300

    add pacote jwt

--- jwt/main.go
+++ jwt/main.go
@@ -1,21 +1,61 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"log"
 
-	"github.com/labstack/echo-jwt/v4"
-	"github.com/labstack/echo/v4"
+	"github.com/golang-jwt/jwt/v4"
 )
 
-func main() {
-	e := echo.New()
-	e.Use(echojwt.JWT([]byte("123456")))
-	e.Use(echojwt.WithConfig(echojwt.Config{
-		SigningKey: []byte("123456"),
-	}))
+const key = "my secure jwt key"
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "hello, World!")
+type UserClaim struct {
+	jwt.RegisteredClaims
+	ID    int
+	Email string
+	Name  string
+}
+
+func main() {
+	jwtToken, err := CreateJWTToken(1, "email@email.com", "First Last")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("JWT Token: %s\n", jwtToken)
+
+	var userClaim UserClaim
+
+	err = ParseJWTToken(jwtToken, &userClaim)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Paersed User Claim: %d %s %s\n", userClaim.ID, userClaim.Email, userClaim.Name)
+}
+
+func CreateJWTToken(id int, email, name string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, UserClaim{
+		RegisteredClaims: jwt.RegisteredClaims{},
+		ID:               id,
+		Email:            email,
+		Name:             name,
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	signedString, err := token.SignedString([]byte(key))
+	if err != nil {
+		return "", fmt.Errorf("error creating signed string: %v", err)
+	}
+	return signedString, nil
+}
+
+func ParseJWTToken(jwtToken string, userClaim *UserClaim) error {
+	token, err := jwt.ParseWithClaims(jwtToken, userClaim, func(token *jwt.Token) (interface{}, error) {
+		return []byte(key), nil
+	})
+	if err != nil {
+		return err
+	}
+	if !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+	return nil
 }
